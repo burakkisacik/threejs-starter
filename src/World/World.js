4e265eaa5674125371b6605e9280e9518649dfe0
burak <burakkisacik@yahoo.com>
2021-05-07 13:47:51 +0300

    orbit control added

--- src/World/World.js
+++ src/World/World.js
@@ -5,6 +5,7 @@ import { createScene } from './components/scene';
 import { createRenderer } from './systems/renderer';
 import { Resizer } from './systems/Resizer';
 import { Loop } from './systems/Loop';
+import { createControls } from './systems/controls';
 
 // These variables are module-scoped: we cannot access them
 // from outside the module
@@ -12,14 +13,19 @@ let camera;
 let renderer;
 let scene;
 let loop;
+let controls;
 
 class World {
   constructor(container) {
     camera = createCamera();
     scene = createScene();
     renderer = createRenderer();
+
     loop = new Loop(camera, scene, renderer);
     container.append(renderer.domElement);
+
+    controls = createControls(camera, renderer.domElement);
+    loop.updatables.push(controls);
 
     const cube = createCube();
     loop.updatables.push(cube);
